4e9a9a64edff05df6430bfbc64f312ea461dfa11
EastWon0103 <dongwon000103@gmail.com>
2023-12-28 00:22:53 +0900

    :sparkles: feat: 닉네임 사용 가능 여부

--- application/user-service/src/main/java/com/fundy/application/user/UserService.java
+++ application/user-service/src/main/java/com/fundy/application/user/UserService.java
@@ -1,8 +1,25 @@
 package com.fundy.application.user;
 
+import com.fundy.application.user.in.IsAvailableNicknameUseCase;
+import com.fundy.application.user.in.dto.res.IsAvailableNicknameResponse;
+import com.fundy.application.user.out.ValidUserPort;
+import com.fundy.domain.user.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class UserService {
+@Transactional(readOnly = true)
+@Slf4j
+@RequiredArgsConstructor
+public class UserService implements IsAvailableNicknameUseCase {
+    private final ValidUserPort validUserPort;
 
+    @Override
+    public IsAvailableNicknameResponse isAvailableNickname(String nickname) {
+        boolean isAvailableNickname = User.validateNickname(nickname) && !validUserPort.existsByNickname(nickname);
+
+        return IsAvailableNicknameResponse.of(nickname, isAvailableNickname);
+    }
 }
